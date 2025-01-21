0f20388a2e7a94ef8f784c8f46903bbc948e2d44
theco <thecodingbee.dev@gmail.com>
2025-01-21 21:03:21 +0100

    Created class MainInterface which serves as the main class, processes given arguments and handles the distribution of data across other classes and methods.

---
+++ src/main/java/com/codingbee/core/MainInterface.java
@@ -0,0 +1,55 @@
+package com.codingbee.core;
+
+import com.codingbee.exceptions.InternalErrorException;
+
+import java.io.IOException;
+
+public class MainInterface {
+    public static void main(String[] args) throws IOException{
+        if (args == null){
+            throw new IllegalArgumentException("Arguments are required: inputPath outputPath + optional" +
+                    " ROMStartingAddress RAMStartingAddress");
+        }
+        if (args.length < 2){
+            throw new IllegalArgumentException("Not enough arguments, required: inputPath outputPath" +
+                    " + optional:  ROMStartingAddress RAMStartingAddress");
+        }
+        if (args.length > 4){
+            throw new IllegalArgumentException("Too many arguments, required: inputPath outputPath + optional:" +
+                    " ROMStartingAddress RAMStartingAddress");
+        }
+        if (args.length == 3){
+            throw new IllegalArgumentException("Two or four arguments required, required arguments: inputPath" +
+                    " outputPath + optional: ROMStartingAddress RAMStartingAddress");
+        }
+        if (args[0] == null || args[1] == null){
+            throw new IllegalArgumentException("Illegal argument: the paths can not be null");
+        }
+
+        String inputPath = args[0];
+        String outputPath = args[1];
+
+        String address1 = "0000";
+        String address2 = "3E80";
+
+        if (args.length == 4){
+            if (args[2] == null || args[3] == null){
+                throw new IllegalArgumentException("Illegal argument: the addresses can not be null");
+            }
+            address1 = args[2];
+            address2 = args[3];
+        }
+
+        String inputCode = FileManager.loadFileAsString(inputPath);
+        String assemblyCode = Compiler.process(inputCode);
+
+        if (assemblyCode == null) {
+            throw new InternalErrorException("The compiler failed to translate the code" +
+                    " to assembly and responded with a null");
+        }
+
+        String outputValues = Assembler.process(assemblyCode, address1, address2);
+
+        FileManager.writeAsmAsBinary(outputValues, outputPath);
+    }
+}
