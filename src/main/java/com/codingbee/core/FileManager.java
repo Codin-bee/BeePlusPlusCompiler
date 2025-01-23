45b9478fc8d3e6fc78d3d2fc22a5a368d3af6252
theco <thecodingbee.dev@gmail.com>
2025-01-23 19:07:26 +0100

    Added methods to load more complex projects with more files from one directory to FileManager

--- src/main/java/com/codingbee/core/FileManager.java
+++ src/main/java/com/codingbee/core/FileManager.java
@@ -1,11 +1,60 @@
 package com.codingbee.core;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileManager {
+    /**
+     * Loads all project files with ".bpp" extension and formats them into one String
+     * @param dirPath path to project directory with all files, can be searched recursively
+     * @return content of all files with ".bpp" extension read as plain text in one String
+     * @throws IOException if the path is not valid, can not be read or does not contain ".bpp" files
+     */
+    public static String loadProject(String dirPath) throws IOException {
+        StringBuilder content = new StringBuilder();
+        File[] bppFiles = listFilesRecursively(dirPath).stream()
+                .filter(file -> file.isFile() && file.getName().endsWith("bpp"))
+                .toArray(File[]::new);
+        for (File bppFile : bppFiles) {
+            content.append(bppFile.getAbsoluteFile()).append("\n");
+            content.append(loadFileAsString(bppFile.getAbsolutePath()));
+            content.append("FILE_END_").append(bppFile.getAbsoluteFile()).append("\n");
+        }
+        return content.toString();
+    }
+
+    /**
+     * Recursively searches directory and all directories inside and returns all non-directory files.
+     * @param directoryPath path of the directory to search
+     * @return all files that are not a directory in the directories and recursively all the directories inside
+     */
+    private static List<File> listFilesRecursively(String directoryPath) {
+        List<File> fileList = new ArrayList<>();
+        File directory = new File(directoryPath);
+
+        if (!directory.exists() || !directory.isDirectory()){
+            throw new IllegalArgumentException("The directory does not exist or is not a valid directory");
+        }
+        File[] files = directory.listFiles();
+        if (files == null){
+            throw new IllegalArgumentException("The directory does not contain any files");
+        }
+        for (File file : files) {
+            if (file.isDirectory()) {
+                fileList.addAll(listFilesRecursively(file.getAbsolutePath()));
+            } else {
+                fileList.add(file);
+            }
+        }
+        return fileList;
+    }
+
+
     /**
      * Loads the text file at given path as one String object
      * @param filePath path of the given file
@@ -14,7 +63,9 @@ public class FileManager {
      */
     public static String loadFileAsString(String filePath) throws IOException {
         try {
-            return new String(Files.readAllBytes(Paths.get(filePath))).replace("\t", "").replace("\r\n", "\n"); // Normalize newlines
+            return new String(Files.readAllBytes(Paths.get(filePath))).replace("\t", "")
+                    .replace("\r\n", "\n");/*Loads information, removes tabs and
+                    normalizes new lines*/
         } catch (IOException e) {
             throw new IOException("Could not read the file " + filePath + ": " + e.getLocalizedMessage());
         }
